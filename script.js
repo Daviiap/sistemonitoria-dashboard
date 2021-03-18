fe3caf41c375305b77ae3668c10928ad6799ffd8
Davi Andrade Pontes <daviiap@outlook.com>
2021-03-17 22:29:29 -0300

    Iniciando criaçao do dashboard

---
+++ script.js
@@ -0,0 +1,38 @@
+const listElement = document.getElementById('submit-list')
+
+const requester = new XMLHttpRequest()
+
+requester.open("GET", "https://api-monitoria-fmccii.herokuapp.com/get", true)
+
+requester.send()
+requester.onreadystatechange = function () {
+  if (requester.readyState == 4 && requester.status == 200) {
+    const response = requester.response;
+
+    for (item of JSON.parse(response)) {
+      const listItem = document.createElement('li')
+      listItem.setAttribute('id', item._id)
+
+      const matricula = document.createElement('span')
+      matricula.innerHTML = item.matricula
+
+      const nome = document.createElement('span')
+      nome.innerHTML = item.nome
+
+      const contato = document.createElement('span')
+      contato.innerHTML = item.contato
+
+      const created_at = document.createElement('span')
+      created_at.innerHTML = new Date(item.created_at).toLocaleString()
+
+      listItem.appendChild(matricula)
+      listItem.appendChild(nome)
+      listItem.appendChild(contato)
+      listItem.appendChild(created_at)
+
+      listElement.append(listItem)
+    }
+  } else {
+    window.alert(`Erro ao fazer requisição!\nStatusCode: ${requester.status}`)
+  }
+}
